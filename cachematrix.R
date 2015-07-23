4aa4caea5d706b771b3cb40bcbd09d9326f5c8d7
karthik-2015 <karthik-2015@users.noreply.github.com>
2015-07-23 05:45:15 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix takes a matrix and caches it as a global variable. It also initializes a global variable y, which is used to later store the inverse.
+## Initializing y here, ensures everytime a new matrix is passed, y is invalidated.
 
-## Write a short comment describing this function
+## This takes a matrix and caches it as a global variable.
 
 makeCacheMatrix <- function(x = matrix()) {
+ y<<- NULL
+  if (is.null(x)) {print ("null matrix, no inverse possible")}
+  x<<- x
 
 }
 
 
-## Write a short comment describing this function
+## This takes the global variable matrix, x, and calculates the inverse. If y is null, it will solve and print, solving first time, else it will print getting cached value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+         if (!is.null(y)) { 
+    print ("getting cached value") 
+                     print (y)}
+  if (is.null(y)) {y<<- solve(x) 
+                   print ("solving first time")
+                   print (y)}
 }
